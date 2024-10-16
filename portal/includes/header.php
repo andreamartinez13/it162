0378431e8bc39d558d07dc07fd97fd516750250e
Andrea E <117130590+andreamartinez13@users.noreply.github.com>
2024-10-15 19:45:29 -0700

    Added php files and fixed the path for portal folder

---
+++ portal/includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Andrea Enriquez M</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/forms.css">
+ </head>
+ <body>
+     <header>
+        <img src="images/bannername.png" alt="bannername" id="banner">
+     <!-- <h1>Andrea Enriquez - Web Design</h1> -->
+        <nav class="topnav" id="myTopnav">
+            <a href="index.html" class="active">Home</a>
+            
+            <!-- <a href="#" target="_blank">Name of project goes here</a> -->
+            <!-- <a href="#" target="_blank">Name of project goes here</a> -->
+            <a href="https://github.com/andreamartinez13/it162" target="_blank">GitHub</a>
+            <a href="https://linkedin.com/in/andrea-enriquez-martinez" target="_blank">LinkedIn</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        </nav>
+     </header>
+     
+   <main class="wrapper">
+        <picture class="profile">
+            <source media="(min-width: 1200px)" srcset="images/andreapicture.JPG">
+            <source media="(min-width: 501px)" srcset="images/andreapicture.JPG">
+            <img src="images/andreapicture.JPG" alt="That's me!">
+        </picture>
+        <!-- Header Ends Here -->
