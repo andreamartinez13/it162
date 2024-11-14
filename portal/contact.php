6fca629aa590a90fda5cdbeafea99ebcf9c5f3f6
andreamartinez13 <andrea.mtz.posta@gmail.com>
2024-11-13 23:14:34 -0800

    Contact Page

--- portal/contact.php
+++ portal/contact.php
@@ -1,3 +1,20 @@
 <?php include 'includes/header.php'; ?>
-<p>Contact Form Goes Here</p>
+<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "andrea.enriquezmar1@seattlecolleges.edu";  //place your/your client's email address here
+        $toName = "AndreaE"; //place your client's name here
+        $website = "andreaenriquez.design";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
 <?php include 'includes/footer.php'; ?>
